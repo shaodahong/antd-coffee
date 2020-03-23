710a5b29625d851ba099fc8fbe2af0419d5fadb6
shaodahong <darryshaw@gmail.com>
2020-03-23 23:54:50 +0800

    fix module.hot

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 
 render(<div>hello world</div>, document.getElementById('root'))
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development' && module.hot) {
   // eslint-disable-next-line no-unused-expressions
   module.hot && module.hot.accept()
 }
