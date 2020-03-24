f245bffd748ec739aaae74876dda40255f0f0286
shaodahong <darryshaw@gmail.com>
2020-03-24 10:42:39 +0800

    judgement NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@
 const merge = require('webpack-merge')
 const baseConf = require('./build/base')
 const devConf = require('./build/dev')
+const buildConf = require('./build/build')
 
-module.exports = merge(baseConf, devConf, {
-  mode: process.env.NODE_ENV,
+const mode = process.env.NODE_ENV
+
+module.exports = merge(baseConf, mode === 'development' ? devConf : buildConf, {
+  mode,
 })
