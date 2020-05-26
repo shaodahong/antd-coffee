8c8fc656a999147857268fd3a59119268cf2619a
shaodahong <darryshaw@gmail.com>
2020-05-26 14:07:04 +0800

    improve: ModalTable body overflow

--- components/modal-table/index.tsx
+++ components/modal-table/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Modal } from 'antd'
 import { ModalProps } from 'antd/lib/modal'
 import Table, { TableProps } from '../table'
+import useWindowSize from '../table/useWindowSize'
 
 export interface ModalTablePorps<RecordType> extends ModalProps {
   tableProps: TableProps<RecordType>
@@ -12,9 +13,18 @@ export default function ModalTable<RecordType extends object>({
   tableProps,
   ...props
 }: ModalTablePorps<RecordType>) {
+  const { height } = useWindowSize()
   return (
-    <Modal footer={null} destroyOnClose {...props}>
-      <Table<RecordType> {...tableProps} />
+    <Modal
+      footer={null}
+      destroyOnClose
+      {...props}
+      bodyStyle={{
+        maxHeight: height,
+        overflow: 'auto',
+      }}
+    >
+      <Table<RecordType> scroll={{ y: undefined }} {...tableProps} />
     </Modal>
   )
 }
